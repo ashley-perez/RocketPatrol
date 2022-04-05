6c6e13c49e13b71a8fc59cb0d68489dd927f45a7
ashley-perez <ashleyperez428@gmail.com>
2022-04-05 15:37:46 -0700

    Added phaser, indexhtml and main file.

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//console.log("hello world");
+
+//const { Phaser } = require("../lib/phaser");
+
+let config = {
+    type: Phaser.CANVAS, 
+    width: 640,
+    height: 480
+}
+
+let game = new Phaser.Game(config);
