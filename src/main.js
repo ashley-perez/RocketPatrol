b7814a14a5907b1462cab9d3ae0996bed5780839
ashley-perez <ashleyperez428@gmail.com>
2022-04-05 20:24:50 -0700

    added border, scrolling starfield, & green UI bar

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-//console.log("hello world");
-
-//const { Phaser } = require("../lib/phaser");
-
+// this creates the canvas for the game (screen size)
 let config = {
     type: Phaser.CANVAS, 
     width: 640,
-    height: 480
+    height: 480,
+    scene: [Menu, Play] // the scenes that we will be using
 }
 
 let game = new Phaser.Game(config);
+
+// setting the UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,8 +4,41 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+
+        // loads images/tiles sprites
+        // load.image() has 2 parameters: string that will be used to reference this image
+        //                                URL for where the image is located
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+
+    }
+    
+    // method
     create() {
-        this.add.text(20, 20, "Rocket Patrol Play");
+        
+        // place tile sprite (starfield background)
+        // add.tileSprite() has 5 parameters: x pos, y pos, width, height, key string for which image to use
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+        
+        // greeen UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
+
+        // white borders
+        // add.rectangle() has 5 parameters: x coord, y coord, width, height and color (hex)
+        // setOrigin() adjusts the rectangles origins according to the given coord
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF0).setOrigin(0,0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+
+    }
+
+    update() {
+
+        // texture of our tile sprite will move 4 horizontal pixels left every frame
+        this.starfield.tilePositionX -= 4;
     }
 
 }
